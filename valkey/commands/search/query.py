466e2fd2c9f15cfecb41677a3105e85360e4d83b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-06-11 23:29:16 -0400

    spelling: ascending
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- valkey/commands/search/query.py
+++ valkey/commands/search/query.py
@@ -296,7 +296,7 @@ class Query:
         Add a sortby field to the query.
 
         - **field** - the name of the field to sort by
-        - **asc** - when `True`, sorting will be done in asceding order
+        - **asc** - when `True`, sorting will be done in ascending order
         """
         self._sortby = SortbyField(field, asc)
         return self
